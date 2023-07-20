6acc7ae91ae2564bc72d8a087daa5a7f2ff23bb8
João Coqueiro <jaaum.p95@gmail.com>
2023-07-20 11:53:47 -0300

    CREATE: Cria esboço do Spotify-Clone

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,23 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+};
+
+export const SpotifyConfig = {
+  clientId: 'f55fae945b364eada546193c73e73667',
+  authEndPoint: 'https://accounts.spotify.com/authorize',
+  redirectUri: 'http://localhost:4200/login/',
+  scopes: [
+    'user-read-currently-playing', // Música atual.
+    'user-read-recently-played', // Música recentemente tocada.
+    'user-read-playback-state', // Ler estado do player de usuário.
+    'user-top-read', // Top artistas e top músicas do user.
+    'user-modify-playback-state', // Alterar o tipo de player do usuário.
+    'user-library-read', // Fazer leitura da biblioteca do user.
+    'playlist-read-private', // Ler playlist's privadas do user.
+    'playlist-read-collaborative', // Ler playlist's colaborativas do user.
+  ],
 };
 
 /*
